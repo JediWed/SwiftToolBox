d28c2056b2f7753b7a04bf564a73680a1b0c7d5f
Ediz Turcan <ediz.turcan@adesso.de>
2021-07-02 08:48:31 +0200

    Value2 Table View Cell created

---
+++ Sources/SwiftToolBox/Extensions/UITableViewCell/STBRightDetailTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  File.swift
+//  
+//
+//  Created by Turcan, Ediz on 02.07.21.
+//
+
+import UIKit
+public class STBRightDetailTableViewCell: UITableViewCell {
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: .value2, reuseIdentifier: reuseIdentifier)
+    }
+    
+    public required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
